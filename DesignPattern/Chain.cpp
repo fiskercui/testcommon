5f94c40b25b803bb2cb2d83c2026cc4cbac9da69
fiskercui <fisker@ubuntu.(none)>
2013-02-17 21:53:16 -0800

    Chain

--- DesignPattern/Chain.cpp
+++ DesignPattern/Chain.cpp
@@ -42,9 +42,13 @@ void HelpHandle::HandleHelp(){
     if(m_successsor != NULL){
         m_successsor->HandleHelp();
     }
+    else
+    {
+        printf("No Help\n");
+    }
 }
 
-
+// widget construction declare protected
 class Widget: public HelpHandle
 {
 public:
@@ -66,14 +70,16 @@ Widget::Widget(Widget* parent, TOPIC t): HelpHandle(parent, t)
 class Button : public Widget
 {
 public:
-    Button(Widget* d, TOPIC t= NO_HELP_TOPIC): Widget(d,t){
-    }
+    Button(Widget* d, TOPIC t= NO_HELP_TOPIC): Widget(d,t){    }
 
     void HandleHelp(){
         std::cout << "Button HandleHelp" << std::endl;
         if(HasHelp())
         {
-
+            printf("Button Help \n");
+            printf("Button Help1\n");
+            printf("Button Help2\n");
+            HelpHandle::HandleHelp();
         }
         else
         {
@@ -94,7 +100,9 @@ public:
         std::cout << "Dialog HandleHelp" << std::endl;
         if(HasHelp())
         {
-
+            printf("Dialog Help\n");
+            printf("Dialog Help1 \n");
+            printf("Dialog Help2 \n");
         }
         else
         {
@@ -112,23 +120,54 @@ public:
     }
 
     virtual void HandleHelp(){
-
+        std::cout << "Application HandleHelp" << std::endl;
+        if(HasHelp())
+        {
+            printf("Application Help\n");
+            printf("Application Help1 \n");
+            printf("Application Help2 \n");
+        }
+        else
+        {
+            HelpHandle::HandleHelp();
+        }
     }
 
 };
 
 
+void HandleChainHelp(HelpHandle* help){
+    if(help != NULL)
+        help->HandleHelp();
+}
+
+//
 int main(int argc, char *argv[])
 {
     const TOPIC PRINT_TOPIC = 1;
     const TOPIC PAPER_ORIENTATION_TOPIC = 2;
     const TOPIC APPLICATION_TOPIC = 3;
 
+    // we can the HelpHandle* point  to the function HandleChainHelp, w
+    // we can define the chain of responsability among Dialog, Button ,Application
+
     Application* pplication =  new Application(APPLICATION_TOPIC);
     Dialog* pDialog = new Dialog(pplication, PRINT_TOPIC);
     Button* pButton = new Button(pDialog, PAPER_ORIENTATION_TOPIC);
 
-    pButton->HandleHelp();
+//    pButton->HandleHelp();
+    printf("process button\n");
+    HandleChainHelp(pButton);
+
+
+    printf("Another Chain\n");
+    Application* pplication2 =  new Application(-1);
+    Dialog* pDialog2 = new Dialog(pplication2, -1);
+    Button* pButton2 = new Button(pDialog2, -1);
+
+
+    printf("process dialog\n");
+    HandleChainHelp(pButton2);
     return 0;
 }
 
---
+++ DesignPattern/Command.cpp
@@ -0,0 +1,188 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <vector>
+#include <algorithm>
+class Command
+{
+public:
+    virtual ~Command(){
+
+    }
+
+    virtual void Excute() = 0;
+protected:
+    Command(){    }
+};
+
+
+class Application;
+class OpenCommand: public Command
+{
+public:
+    OpenCommand(Application*);
+
+    virtual void Excute();
+
+protected:
+    virtual const char* AskUser();
+
+private:
+    Application* m_app;
+    char* response;
+};
+
+
+OpenCommand::OpenCommand(Application* app)
+{
+    m_app = app;
+}
+
+
+void OpenCommand::Excute()
+{
+    const char* name = AskUser();
+    if(name != NULL)
+    {
+
+    }
+
+}
+
+const char* OpenCommand::AskUser()
+{
+    return NULL;
+}
+
+
+
+class Document;
+class PasteCommand:public Command
+{
+public:
+    PasteCommand(Document *);
+    virtual ~PasteCommand();
+
+    virtual void Excute();
+private:
+    Document* m_doc;
+};
+
+PasteCommand::PasteCommand(Document* doc): m_doc(doc){
+
+}
+
+PasteCommand::~PasteCommand(){
+    m_doc = NULL;
+}
+
+
+void PasteCommand::Excute()
+{
+
+}
+
+template<class Receiver>
+class SimpleCommand : public Command
+{
+public:
+    typedef void (Receiver::*Action)();
+    SimpleCommand(Receiver* r, Action a): m_rece(r), m_act(a){
+    }
+    virtual ~SimpleCommand() {
+
+    }
+
+    virtual void Excute();
+private:
+    Action m_act;
+    Receiver* m_rece;
+};
+
+template <class Receiver>
+void SimpleCommand<Receiver>::Excute()
+{
+    (m_rece->*m_act)();
+}
+
+class MyClass
+{
+public:
+    MyClass(){
+
+    }
+    virtual ~MyClass(){
+
+    }
+
+    void Action(){
+        printf("action\n");
+    }
+};
+
+class MacroCommand:public Command
+{
+public:
+    MacroCommand(){
+
+    }
+    virtual ~MacroCommand(){
+
+    }
+
+    virtual void Add(Command*);
+    virtual void Remove(Command*);
+
+    virtual void Excute();
+
+protected:
+    bool HasCommand(Command*);
+
+private:
+    std::vector<Command* > m_vecCommand;
+};
+
+
+bool MacroCommand::HasCommand(Command* cmd)
+{
+    std::vector<Command* >::iterator it = std::find (m_vecCommand.begin(), m_vecCommand.end(), cmd);
+    if(it != m_vecCommand.end())
+        return true;
+    else
+        return false;
+}
+
+void MacroCommand::Add(Command* cmd)
+{
+    if(HasCommand(cmd))
+        Remove(cmd);
+    m_vecCommand.push_back(cmd);
+}
+
+void MacroCommand::Remove(Command* cmd)
+{
+    std::vector<Command* >::iterator it = std::find (m_vecCommand.begin(), m_vecCommand.end(), cmd);
+    if(it != m_vecCommand.end())
+        m_vecCommand.erase(it);
+}
+
+void MacroCommand::Excute()
+{
+    std::vector<Command* >::iterator it ;
+    for(it = m_vecCommand.begin(); it!= m_vecCommand.end(); it++)
+    {
+        (*it)->Excute();
+    }
+}
+
+
+
+
+int main(int argc, char *argv[])
+{
+    MyClass* receiver = new MyClass;
+
+    Command* aCommand = new SimpleCommand<MyClass>(receiver, &MyClass::Action);
+
+    aCommand->Excute();
+    return 0;
+}
